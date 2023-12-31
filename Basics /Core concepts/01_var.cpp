ce3fae07ed3766cf89154c91dba14c0b7ca3c964
Ujjwl <104405727+Ujjwlkushwaha@users.noreply.github.com>
2023-12-31 03:55:54 +0000

    update file

---
+++ Basics /Core concepts/01_var.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // These all are variables in c++
+
+    int num = 10 ; // interger variable
+    float f = 23.4 ; // float variable
+    char c = 'A' ; // character variable
+    string str = "Ujjwal kushwaha"; // string 
+    double d = 23.54545;
+
+    // cout used for printing something 
+    cout << "My name is " << str << endl;
+
+}
